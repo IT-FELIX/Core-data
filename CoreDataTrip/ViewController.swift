fba6f74dea898d3d4200e8cd2d250b7999448f7e
SRS <srs@srss-macbook-pro.local>
2019-10-14 11:39:15 +0530

    Initial Commit

---
+++ CoreDataTrip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataTrip
+//
+//  Created by SRS on 14/10/19.
+//  Copyright © 2019 Harish Kshirsagar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
